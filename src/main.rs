15534905f57bb1c70e82cb5091691d04ee12629f
Subsequent <software@subseq.io>
2024-02-22 05:31:37 +0000

    Task complete

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-use actix_web::{web, HttpServer, App};
-use rize::login_handler;
-use sqlx::PgPool;
-use dotenv::dotenv;
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
+
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let db_pool = PgPool::connect_lazy(&database_url).unwrap();
     let data_pool = web::Data::new(db_pool);
+
     HttpServer::new(move || {
         App::new()
             .app_data(data_pool.clone())
             .service(web::resource("/login").route(web::post().to(login_handler)))
+            .service(web::resource("/user").route(web::get().to(get_user_profile)))
+            .service(web::resource("/user").route(web::put().to(update_user_profile)))
     })
     .bind("127.0.0.1:8080")?
     .run()
